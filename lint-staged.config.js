5068c0f7d19bd65f6e79e2fd524bad40a71fdc70
Colin Hemphill <hemphill.colin@gmail.com>
2021-05-14 13:48:10 -0500

    Dependency upgrades. Fixing husky and lint-staged setup.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  '*.@(js|ts|tsx)': ['eslint --fix', 'prettier --write'],
-  '*.@*(scss)': 'stylelint --syntax=scss',
+  '*.scss': 'stylelint --syntax="scss',
+  '*.{js,jsx,ts,tsx,md,html,css}': 'prettier --write',
+  '*.{js,jsx,ts,tsx}': 'eslint --fix',
 };
