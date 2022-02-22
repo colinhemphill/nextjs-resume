42a1e0feb4023f3bfe7cdd8c360b75c1bc34ee2d
Colin Hemphill <colin@colinhemphill.com>
2022-02-22 13:16:57 -0600

    Stars.

--- src/components/StarRating/StarRating.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const star2 = style({
-  opacity: 0.75,
-});
-
-export const star3 = style({
-  opacity: 0.5,
-});
--- src/components/StarRating/StarRating.tsx
+++ src/components/StarRating/StarRating.tsx
@@ -2,32 +2,23 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import colors from '../../strum-design-system/themes/timbre/colors';
-import { star2, star3 } from './StarRating.css';
 
 interface StarRatingProps {
   stars: 1 | 2 | 3;
 }
+
+const Star: React.FC<{ className?: string }> = ({ className }) => {
+  return <FontAwesomeIcon color={colors.yellow} icon={faStar} size="sm" />;
+};
 
 const StarRating: React.FC<StarRatingProps> = (props) => {
   const { stars } = props;
 
   return (
     <span>
-      {stars >= 3 && <FontAwesomeIcon color={colors.primary} icon={faStar} />}
-      {stars >= 2 && (
-        <FontAwesomeIcon
-          className={star2}
-          color={colors.primary}
-          icon={faStar}
-        />
-      )}
-      {stars >= 1 && (
-        <FontAwesomeIcon
-          className={star3}
-          color={colors.primary}
-          icon={faStar}
-        />
-      )}
+      {stars >= 1 && <Star />}
+      {stars >= 2 && <Star />}
+      {stars >= 3 && <Star />}
     </span>
   );
 };
