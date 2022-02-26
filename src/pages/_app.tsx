f1a50a4fc395ab5c4d01490ed44f59aff46329a1
Colin Hemphill <colin@colinhemphill.com>
2022-02-26 08:36:16 -0600

    Manifest.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
 
       <ErrorBoundary>
