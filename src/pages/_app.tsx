8c2ce335b2c4cf2746491773cd46ca714b4aa29d
Colin Hemphill <colin@colinhemphill.com>
2022-02-26 08:40:53 -0600

    Theme color.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import '../helpers/fontawesomeConfig';
 import '../strum-design-system';
 import StrumProvider from '../strum-design-system/components/StrumProvider/StrumProvider';
+import colors from '../strum-design-system/themes/timbre/colors';
 import '../styles/app.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
@@ -13,6 +14,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content={colors.primary} />
         <link rel="manifest" href="/manifest.json" />
       </Head>
 
