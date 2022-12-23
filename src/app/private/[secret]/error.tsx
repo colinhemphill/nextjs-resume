1cbe07f9c93ec399ac259a60bac47f2550415fda
Colin Hemphill <colin.hemphill@bit.ly>
2022-12-23 15:49:37 -0600

    Error message.

--- src/app/private/[secret]/error.tsx
+++ src/app/private/[secret]/error.tsx
@@ -3,12 +3,12 @@
 import Link from 'next/link';
 import { Heading } from '../../../components/Heading/Heading';
 
-export default function Error({ error }: { error: Error }) {
+export default function Error() {
   return (
     <div className="container mt-12 text-center">
       <div className="rounded-xl bg-neutral-light-3 py-12 dark:bg-neutral-dark-3">
         <Heading color="error11" level={2}>
-          {error.message}
+          Not authorized
         </Heading>
 
         <div className="mt-4">
