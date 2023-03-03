02e29eba7f04acaadf892bea2a1d9dd1923c21e2
Colin Hemphill <colinhemphill@users.noreply.github.com>
2023-03-02 21:48:52 -0600

    [3.2.1] Dependency upgrades (#221)
    * Upgrades. Remove bundle analyzer plugin.
    
    * Cleanup.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
-import bundleAnalyzer from '@next/bundle-analyzer';
-import withPlugins from 'next-compose-plugins';
-
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { appDir: true },
@@ -12,6 +5,4 @@ const nextConfig = {
   swcMinify: true,
 };
 
-const plugins = [[withBundleAnalyzer]];
-
-export default withPlugins(plugins, nextConfig);
+export default nextConfig;
