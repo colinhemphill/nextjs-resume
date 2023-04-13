11b43eedae8a58cbec991143d8ff0a87f4dd95d1
Colin Hemphill <colin.hemphill@bit.ly>
2023-04-12 22:47:15 -0500

    Test path alias.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@content': path.resolve(__dirname, './.contentlayer/generated'),
+      '@test-utils': path.resolve(__dirname, './src/test-utils.tsx'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
