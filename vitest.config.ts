22cfe1817f6c8f6fb4d958450a07a38a2053e781
Colin Hemphill <colinhemphill@users.noreply.github.com>
2023-08-11 13:17:19 -0500

    [4.1.1] Dependency upgrades (#341)
    * Upgrades. Updating to Radix Colors 2.x may result in changes to your color choices.
    
    * Use a plugin to infer tsconfig paths in Vitest.
    
    * Update changelog.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,11 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@content': path.resolve(__dirname, './.contentlayer/generated'),
-      '@test-utils': path.resolve(__dirname, './src/test-utils.tsx'),
-    },
-  },
+  plugins: [tsconfigPaths(), react()],
   test: {
     environment: 'jsdom',
     globals: true,
