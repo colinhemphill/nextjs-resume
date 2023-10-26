01992b374c9a958e162a6ffafc015e6da02c742b
Colin Hemphill <colinhemphill@users.noreply.github.com>
2023-10-26 13:49:58 -0500

    [4.2.2] Upgrade to Next.js 14 (#395)
    * Next monorepo updates.
    
    * Upgrade to Next.js 14.

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from 'next/server';
+import { ImageResponse } from 'next/og';
 import { contrastColor } from '../helpers/colorContrast';
 import { getAccentColor } from '../helpers/colors';
 
