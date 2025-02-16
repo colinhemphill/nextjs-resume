805bd271e2f379089c9ab01720e197e21062e09d
Colin Hemphill <colin.hemphill@bit.ly>
2025-02-16 09:22:32 -0600

    Update OG image designs

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,5 +1,8 @@
 import { resumeConfig } from '@config/resume-config';
+import { personal } from '@content';
 import { ImageResponse } from 'next/og';
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
 import { getAccentColor, getNeutralColor } from 'src/helpers/colors';
 import { fullName } from 'src/helpers/utils';
 
@@ -7,29 +10,30 @@ const theme = resumeConfig.imageTheme;
 
 export const alt = 'Professional Résumé';
 export const contentType = 'image/png';
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 export const size = {
   width: 1200,
   height: 630,
 };
 
-export default function og(): ImageResponse {
+export default async function og(): Promise<ImageResponse> {
+  const albertBold = await readFile(
+    path.join(process.cwd(), 'src/fonts/AlbertSans-Bold.ttf'),
+  );
+  const albertNormal = await readFile(
+    path.join(process.cwd(), 'src/fonts/AlbertSans-Regular.ttf'),
+  );
+
   return new ImageResponse(
     (
       <div
         style={{
-          backgroundColor: getNeutralColor(1, theme),
+          fontFamily: 'AlbertSans',
+          background: `linear-gradient(to right bottom, ${getNeutralColor(1, theme)} 0%, ${getAccentColor(4, theme)} 50%, ${getAccentColor(6, theme)} 100%)`,
         }}
-        tw="flex h-full w-full items-center justify-center"
+        tw="flex h-full w-full flex-col justify-between p-20"
       >
-        <div
-          style={{
-            backgroundColor: getAccentColor(2, theme),
-            color: getAccentColor(11, theme),
-            borderColor: getAccentColor(7, theme),
-          }}
-          tw="flex h-8/12 w-9/12 flex-col items-center justify-center rounded-xl border-2 text-center"
-        >
+        <div tw="flex flex-col">
           <div
             style={{
               color: getAccentColor(12, theme),
@@ -42,19 +46,37 @@ export default function og(): ImageResponse {
           <div
             style={{
               color: getAccentColor(11, theme),
+              fontWeight: 700,
             }}
-            tw="text-6xl"
+            tw="mt-4 text-6xl"
           >
-            Professional Résumé
+            {personal.title}
           </div>
-          <div
-            style={{ color: getAccentColor(12, theme) }}
-            tw="mt-12 rounded-lg text-4xl"
-          >
-            Available online or as a PDF
-          </div>
+        </div>
+        <div
+          style={{ color: getAccentColor(12, theme) }}
+          tw="rounded-lg text-4xl"
+        >
+          Professional résumé available online or as a PDF
         </div>
       </div>
     ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: 'AlbertSans',
+          data: albertBold,
+          style: 'normal',
+          weight: 700,
+        },
+        {
+          name: 'AlbertSans',
+          data: albertNormal,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    },
   );
 }
