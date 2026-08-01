879be5551da4367181860511f20d75527daa4155
Colin Hemphill <colin.hemphill@bit.ly>
2026-08-01 09:24:25 -0500

    Upgrade TypeScript.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { NextConfig } from 'next';
 import { withContentlayer } from 'next-contentlayer2';
 
 const nextConfig: NextConfig = {
+  experimental: { useTypeScriptCli: true },
   reactStrictMode: true,
   reactCompiler: true,
   turbopack: {},
